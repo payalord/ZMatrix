df417e741635cb013e3dcacbc675f4751da66992
Payalord <payalord@gmail.com>
2020-02-09 22:15:35 -0500

    Prepare for new installer

--- CompleteDistroNT.bat
+++ CompleteDistroNT.bat
@@ -1,32 +1,5 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE";"C:\Program Files (x86)\Inno Setup 6";"C:\Program Files (x86)\HTML Help Workshop";"C:\Program Files (x86)\Borland\CBuilder6\Bin\";"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86"
 
-if not "%1" == "" (
-	if exist scripts (
-	cd scripts
-	call ReVersionAll %1
-	cd ..
-	)
-)
-
-devenv matrix.sln /rebuild Release /project "ZMatrixSS"
-
-if exist Config (
-cd Config
-bpr2mak -tdefault.bmk Config.bpr
-make -B -f Config.mak
-cd ..
-)
-
-if exist Help\help.zmatrix.n3.net\src (
-cd Help\help.zmatrix.n3.net\src
-call make
-if exist ..\release (
-cd ..\release
-hhc ZMatrixHelp.hhp
-)
-cd ..\..\..
-)
-
 rmdir /S /Q DistroNT
 mkdir DistroNT
 
@@ -47,9 +20,6 @@ copy WinampVis\vis_zmx.dll DistroNT
 mkdir DistroNT\ScreenSaver
 
 copy ScreenSaver\ZMatrixSS.scr DistroNT\ScreenSaver
-
-devenv matrix.sln /clean Release /project "ZMatrixSS"
-REM del /Q Config.dll
 
 if exist Setup (
 cd Setup
